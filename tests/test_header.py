fc66213817edc2f388b217e628696d39556ba7f4
Matthieu Gautier <mgautier@kymeria.fr>
2019-02-25 17:29:24 +0100

    Add the license.

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,3 +1,20 @@
+# coding=utf-8
+
+# This file is part of pyzim-tools.
+#
+# pyzim-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# pyzim-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyzim-tools.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import pytest
 import pyzim
 
